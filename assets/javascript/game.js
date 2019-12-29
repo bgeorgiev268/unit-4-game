8ff51b75fd29a446ba5480f61b059b9147b3815c
bgeorgiev268 <bgeorgiev268@gmail.com>
2019-12-29 01:36:07 -0800

    added onclick functions for the crystals on game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,36 +8,60 @@
 
 // When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
 //on click function for each crystal 
-function newGame()
-function loss()
-function win()
-function reset()
+// function newGame()
+// function loss()
+// function win()
+// function reset()
 
 // variables
 var wins = 0;
 var losses = 0;
-var score = 0;
-var randomNumber = [];
-var blue = [];
-var green = [];
-var red = [];
-var yellow = [];
+var currentScore = 0;
+var targetScore = 0;
+var blue = 0;
+var green = 0;
+var red = 0;
+var yellow = 0;
+// var crystal = {
+//     blue:
+//     {
+//         name: "Blue"
+//         value: 0
+//     },
+//     green:
+//     {
+//         name: "Green"
+//         value:  0
+//     },
+//     red:
+//     {
+//         name: "Red"
+//         value = 0
+//     },
+//     yellow:
+//     {
+//         name: "Yellow"
+//         value: 0
+//     }
+// };
 
 // Your game will hide this amount until the player clicks a crystal.
 // When they do click one, update the player's score counter.
-
-
-
 // The player wins if their total score matches the random number from the beginning of the game.
-
-
 // The player loses if their score goes above the random number.
-
-
 // The game restarts whenever the player wins or loses.
-
 // When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
-
-
-
 // The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
+
+$('#one').on ('click', function(){
+    alert("test")
+}); 
+$('#two').on ('click', function(){
+    alert("test")
+}); 
+$('#three').on ('click', function(){
+    alert("test")
+}); 
+$('#four').on ('click', function(){
+    alert("test")
+}); 
