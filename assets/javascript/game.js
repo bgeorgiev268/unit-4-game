1a981d7a34ccb4756f198c4b0e6a5ec85455f05c
bgeorgiev268 <bgeorgiev268@gmail.com>
2019-12-30 21:23:28 -0800

    completed js file and changed box layout

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,28 +22,28 @@ var blue = 0;
 var green = 0;
 var red = 0;
 var yellow = 0;
-// var crystal = {
-//     blue:
-//     {
-//         name: "Blue"
-//         value: 0
-//     },
-//     green:
-//     {
-//         name: "Green"
-//         value:  0
-//     },
-//     red:
-//     {
-//         name: "Red"
-//         value = 0
-//     },
-//     yellow:
-//     {
-//         name: "Yellow"
-//         value: 0
-//     }
-// };
+var crystal = {
+    blue:
+    {
+        name: "Blue",
+        value: 0
+    },
+    green:
+    {
+        name: "Green",
+        value: 0
+    },
+    red:
+    {
+        name: "Red",
+        value: 0
+    },
+    yellow:
+    {
+        name: "Yellow",
+        value: 0
+    }
+};
 
 // Your game will hide this amount until the player clicks a crystal.
 // When they do click one, update the player's score counter.
@@ -53,15 +53,80 @@ var yellow = 0;
 // When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
 // The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
 
-$('#one').on ('click', function(){
-    alert("test")
-}); 
-$('#two').on ('click', function(){
-    alert("test")
-}); 
-$('#three').on ('click', function(){
-    alert("test")
-}); 
-$('#four').on ('click', function(){
-    alert("test")
-}); 
+//Functions 
+//======================================================
+
+
+// Helper function for getting random numbers
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// starts the game (and restarts the game)
+function startGame() {
+    //Reset current score
+    currentScore = 0;
+    $("#currentnumber").text(currentScore)
+    //Set new target score (19-120)
+    targetScore = getRandom(19, 120)
+    $("#randomNumber").text(targetScore)
+    // set different values for each crystal (1-12)
+    crystal.blue.value = getRandom(1, 12)
+    crystal.green.value = getRandom(1, 12)
+    crystal.red.value = getRandom(1, 12)
+    crystal.yellow.value = getRandom(1, 12)
+
+    // change HTML to reflect all of these changes
+    // testing console
+    console.log("----------------")
+    console.log("Target Score: " + targetScore);
+    console.log("Blue: " + crystal.blue.value + " | Red: " + crystal.green.value + " | Green " + crystal.green.value + " | Yellow " + crystal.yellow.value);
+    console.log("----------------")
+
+}
+// crystal click events
+//======================================================
+$('#one').on('click', function () {
+    currentScore = currentScore + crystal.blue.value
+    console.log(currentScore)
+    verifyScore()
+});
+$('#two').on('click', function () {
+    currentScore = currentScore + crystal.green.value
+    console.log(currentScore)
+    verifyScore()
+
+});
+$('#three').on('click', function () {
+    currentScore = currentScore + crystal.red.value
+    console.log(currentScore)
+    verifyScore()
+
+});
+$('#four').on('click', function () {
+    currentScore = currentScore + crystal.yellow.value
+    console.log(currentScore)
+    verifyScore()
+
+});
+
+
+
+function verifyScore() {
+    $("#currentnumber").text(currentScore)
+    if (currentScore === targetScore) {
+        wins++
+        $("#wins").text(wins)
+        alert("You Win!")
+        startGame()
+    }
+    else if (currentScore > targetScore) {
+        losses++
+        $("#losses").text(losses)
+        alert("You Loose!")
+        startGame()
+    }
+}
+
+//starts the game the first time
+startGame()
