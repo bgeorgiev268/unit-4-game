946508ee19f6c33229377226c319adb6d160d274
bgeorgiev268 <bgeorgiev268@gmail.com>
2020-01-03 22:15:55 -0800

    cleaned up js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,8 @@
 // There will be four crystals displayed as buttons on the page.
-//done with html and css
 
 // The player will be shown a random number at the start of the game.
-//Math.floor(Math.random())
-
-
 
 // When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
-//on click function for each crystal 
 
 // variables
 var wins = 0;
@@ -41,17 +36,9 @@ var crystal = {
     }
 };
 
-// Your game will hide this amount until the player clicks a crystal.
-// When they do click one, update the player's score counter.
-// The player wins if their total score matches the random number from the beginning of the game.
-// The player loses if their score goes above the random number.
-// The game restarts whenever the player wins or loses.
-// When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
-// The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
 
 //Functions 
 //======================================================
-
 
 // Helper function for getting random numbers
 function getRandom(min, max) {
@@ -71,17 +58,10 @@ function startGame() {
     crystal.green.value = getRandom(1, 12)
     crystal.red.value = getRandom(1, 12)
     crystal.yellow.value = getRandom(1, 12)
-
-    // change HTML to reflect all of these changes
-    // testing console
-    console.log("----------------")
-    console.log("Target Score: " + targetScore);
-    console.log("Blue: " + crystal.blue.value + " | Red: " + crystal.green.value + " | Green " + crystal.green.value + " | Yellow " + crystal.yellow.value);
-    console.log("----------------")
-
 }
 // crystal click events
 //======================================================
+
 $('#one').on('click', function () {
     currentScore = currentScore + crystal.blue.value
     console.log(currentScore)
